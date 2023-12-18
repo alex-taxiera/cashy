512a7aff67cf358e19d2d7e8b64c0fbc95467bcb
Alex Taxiera <alex.taxiera@paperlessparts.com>
2023-12-17 22:02:36 -0500

    reset migrations

--- prisma/migrations/20231215154415_add_plaid_item_id_index/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[itemId]` on the table `PlaidItem` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "PlaidItem_itemId_key" ON "PlaidItem"("itemId");
